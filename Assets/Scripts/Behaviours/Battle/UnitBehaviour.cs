f93a1a00cd149bb54b825f28862695508c90b244
DanWfram <100808263+DanWfram@users.noreply.github.com>
2023-10-20 02:22:33 +0200

    arreglo corrutinas

--- Assets/Scripts/Behaviours/Battle/UnitBehaviour.cs
+++ Assets/Scripts/Behaviours/Battle/UnitBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Xml.Linq;
+using UnityEngine;
 public class UnitBehaviour : MonoBehaviour
 {    
     public element type;//el elemento de la unidad
@@ -16,25 +17,9 @@ public class UnitBehaviour : MonoBehaviour
         }
     }
     private int currentHealth; //vida actual
+    private Coroutine hitCoroutine;
     public int maxHealth; // vida maxima
     public bool isAlly; //informacion de bando de la unidad
-    public void DamageFormula(int eDamage, element eElement)
-    {
-        //REHACER 
-        //con operadores elvis para los extremos del enum elemental
-        if (eElement == (type + 1 == (element)4 ? element.water : type + 1)) //elemento al que es debil
-        {
-            health -= eDamage * 2;//doble de da�o, ajustar si es necesario
-        }
-        else if (eElement == (type - 1 == (element)(-1) ? element.fire : type - 1))  //elemento al que es fuerte
-        {
-            health -= (int)( eDamage * 0.5f);//mitad de da�o, ajustar si es necesario
-        }
-        else //elemento igual o neutral
-        {
-            health -= eDamage;//neutral
-        }
-    }
     public void Start()
     {
         currentHealth = maxHealth;//inicializar la vida al maximo
@@ -45,7 +30,24 @@ public class UnitBehaviour : MonoBehaviour
         UnitBehaviour unit = collision.gameObject.GetComponent<UnitBehaviour>();
         if (fighter != null && unit.isAlly != isAlly)//si choca un enemigo
         {
-            StartCoroutine(fighter.hitClock.Cycle(() => DamageFormula(fighter.damage,unit.type)));//empieza corrutina de reloj que aplica el da�o como una expresion lambda
+            hitCoroutine = StartCoroutine(fighter.hitClock.Cycle(() => 
+            {
+                //CODIGO DE DAÑO, PROVISIONAL/////
+                //con operadores elvis para los extremos del enum elemental
+                if (unit.type == (type + 1 == (element)4 ? element.water : type + 1)) //elemento al que es debil
+                {
+                    health -= fighter.damage * 2;//doble de da�o, ajustar si es necesario
+                }
+                else if (unit.type == (type - 1 == (element)(-1) ? element.fire : type - 1))  //elemento al que es fuerte
+                {
+                    //SI EL DAÑO ES MENOR A 2 SE HACE 0
+                    health -= (int)(fighter.damage * 0.5f);//mitad de da�o, ajustar si es necesario
+                }
+                else //elemento igual o neutral
+                {
+                    health -= fighter.damage;//neutral
+                }
+            }));//empieza corrutina de reloj que aplica el da�o como una expresion lambda
         }
     }
     private void OnCollisionExit2D(Collision2D collision)// si TERMINA de colisionar
@@ -54,7 +56,7 @@ public class UnitBehaviour : MonoBehaviour
         UnitBehaviour unit = collision.gameObject.GetComponent<UnitBehaviour>();
         if (fighter != null && unit.isAlly != isAlly)
         {
-            StopCoroutine(fighter.hitClock.Cycle(() => DamageFormula(fighter.damage, unit.type)));//termina la corrutina de reloj empezada
+            StopCoroutine(hitCoroutine);//termina la corrutina de reloj empezada
         }
     }
 }
