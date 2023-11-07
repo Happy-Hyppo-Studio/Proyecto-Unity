479de3ec00f43deb4836febec88d3608260c2946
Yago Ortiz <115984356+Kaynt-manage@users.noreply.github.com>
2023-11-07 14:38:00 +0100

    Opciones de juego
    Se a añadido un slider que cambia el mixer de volumen, un boton para pantalla completa (solo se puede probar en la build) y un desplegable para seleccionar calidad

--- Assets/Scripts/Behaviours/UI/OptionsMenuBehaviour.cs
+++ Assets/Scripts/Behaviours/UI/OptionsMenuBehaviour.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsMenuBehaviour : MonoBehaviour
 {
     public Button backButton, audioButton, controlsButton;
     public GameObject audioOptions, controlsOptions, pauseMenu;
-    // Start is called before the first frame update
+
+
+    public AudioMixer audioMixer;
+
     void Start()
     {
         backButton.onClick.AddListener(() => {
@@ -22,6 +26,22 @@ public class OptionsMenuBehaviour : MonoBehaviour
             controlsOptions.SetActive(true);
             audioOptions.SetActive(false);
         });
+    }
+
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 
 }
