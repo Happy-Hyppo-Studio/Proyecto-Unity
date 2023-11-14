03acecfa983c26af7e52ef5a1a5e9e1c440e4187
DanWfram <100808263+DanWfram@users.noreply.github.com>
2023-11-14 23:46:07 +0100

    NIVELES 3 Y 4

--- Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
+++ Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class PlanLineBehaviour : MonoBehaviour
 {
     //SI TENEIS UNA IDEA QUE PENSAIS ES MAS RENTABLE PARA LA CREACION DE NIVELES NO DUDEIS EN EXPERIMENTARLA, ESTA ES LA QUE OS PUEDO DEJAR COMO TEMPLATE
+    public GameObject enemy;
     public int[] timeOffsets = {/*meter aqui los tiempos de en que momento se genera un nuevo enemigo en la linea segun el plan, en segundos, de menor a mayor*/};
     int i = 0;
     // Update is called once per frame
     void Update()//mucha pereza ahora para hacer corrutinas, optimizare mas tarde
     {
-        if (Time.timeSinceLevelLoad > timeOffsets[i])
+        if (Time.timeSinceLevelLoad > timeOffsets[i] && i < timeOffsets.Length -1)
         {
-            Instantiate(this.gameObject/*enemigo, this es placecolder*/, Vector3.zero, Quaternion.identity /*unir este script a un gameobject vacio delante de las torres enemigas*/);
+            Instantiate(enemy, transform.position, Quaternion.identity /*unir este script a un gameobject vacio delante de las torres enemigas*/);
             i++;
         }
 
