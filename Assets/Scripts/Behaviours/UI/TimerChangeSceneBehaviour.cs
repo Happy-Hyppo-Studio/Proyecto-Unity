95dbd0a4850b075669d36c2875944a295f030035
DanielBermejoValverde <43214261+DanielBermejoValverde@users.noreply.github.com>
2023-11-18 02:29:41 +0100

    Tienda y últimas mejoras

--- Assets/Scripts/Behaviours/UI/TimerChangeSceneBehaviour.cs
+++ Assets/Scripts/Behaviours/UI/TimerChangeSceneBehaviour.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 public class TimerChangeSceneBehaviour : MonoBehaviour
 {
     public int scene;
-    public Counter clock;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(clock.StartCountdown());
-        SceneManager.LoadScene(scene);
+        
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(scene);
+        }
     }
 }
