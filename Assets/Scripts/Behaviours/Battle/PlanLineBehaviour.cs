a3bfc86b34ad946a1e0a9971f892c350f5bdc053
DanWfram <100808263+DanWfram@users.noreply.github.com>
2023-11-18 19:40:42 +0100

    optimizado y dada una interfaz limpio de
    y corregido dialogs

--- Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
+++ Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlanLineBehaviour : MonoBehaviour
 {
+    [System.Serializable]
+    public struct formationUnit
+    {
+        public GameObject enemy;
+        public float secondsSincePrevious;
+    }
     //SI TENEIS UNA IDEA QUE PENSAIS ES MAS RENTABLE PARA LA CREACION DE NIVELES NO DUDEIS EN EXPERIMENTARLA, ESTA ES LA QUE OS PUEDO DEJAR COMO TEMPLATE
-    public GameObject[] enemy;
-    public int[] timeOffsets = {/*meter aqui los tiempos de en que momento se genera un nuevo enemigo en la linea segun el plan, en segundos, de menor a mayor*/};
-    int i = 0;
+    public formationUnit[] formation;
+    private void Start()
+    {
+        StartCoroutine(roundLoop());
+    }
+    /*
     // Update is called once per frame
     void Update()//mucha pereza ahora para hacer corrutinas, optimizare mas tarde
     {
         if (Time.timeSinceLevelLoad > timeOffsets[i] && i < timeOffsets.Length -1)
         {
-            Instantiate(enemy[i%enemy.Length], transform.position, Quaternion.identity /*unir este script a un gameobject vacio delante de las torres enemigas*/);
+            Instantiate(enemy[i%enemy.Length], transform.position, Quaternion.identity);
             i++;
         }
 
+    }*/
+    IEnumerator roundLoop()
+    {
+        for (int i = 0; i < formation.Length; i++)
+        {
+            yield return new WaitForSeconds(formation[i].secondsSincePrevious);
+            Instantiate(formation[i].enemy, transform.position, Quaternion.identity);            
+        }
     }
 }
