45eb4a9037dae822d951bc4cfe02990a09cb7052
DanWfram <100808263+DanWfram@users.noreply.github.com>
2023-11-18 21:54:43 +0100

    Unico archivo de planeacion de niveles

--- Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
+++ Assets/Scripts/Behaviours/Battle/PlanLineBehaviour.cs
@@ -4,35 +4,29 @@ using UnityEngine;
 
 public class PlanLineBehaviour : MonoBehaviour
 {
+    //SI TENEIS UNA IDEA QUE PENSAIS ES MAS RENTABLE PARA LA CREACION DE NIVELES NO DUDEIS EN EXPERIMENTARLA, ESTA ES LA QUE OS PUEDO DEJAR COMO TEMPLATE
     [System.Serializable]
     public struct formationUnit
     {
         public GameObject enemy;
         public float secondsSincePrevious;
-    }
-    //SI TENEIS UNA IDEA QUE PENSAIS ES MAS RENTABLE PARA LA CREACION DE NIVELES NO DUDEIS EN EXPERIMENTARLA, ESTA ES LA QUE OS PUEDO DEJAR COMO TEMPLATE
+    }   
     public formationUnit[] formation;
     private void Start()
     {
         StartCoroutine(roundLoop());
     }
-    /*
-    // Update is called once per frame
-    void Update()//mucha pereza ahora para hacer corrutinas, optimizare mas tarde
-    {
-        if (Time.timeSinceLevelLoad > timeOffsets[i] && i < timeOffsets.Length -1)
-        {
-            Instantiate(enemy[i%enemy.Length], transform.position, Quaternion.identity);
-            i++;
-        }
-
-    }*/
     IEnumerator roundLoop()
     {
-        for (int i = 0; i < formation.Length; i++)
-        {
-            yield return new WaitForSeconds(formation[i].secondsSincePrevious);
-            Instantiate(formation[i].enemy, transform.position, Quaternion.identity);            
-        }
+        Vector3 spawnPoint = (GetComponent<UnitBehaviour>().isAlly) ? this.transform.position + new Vector3(1, 0, 0) : this.transform.position + new Vector3(-1, 0, 0);
+        while (true)
+        {            
+            for (int i = 0; i < formation.Length; i++)
+            {
+                yield return new WaitForSeconds(formation[i].secondsSincePrevious);
+                Instantiate(formation[i].enemy, spawnPoint, Quaternion.identity);
+            }
+            yield return null;
+        }       
     }
 }
