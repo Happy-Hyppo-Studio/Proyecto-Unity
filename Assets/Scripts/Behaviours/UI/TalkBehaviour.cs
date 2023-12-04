a251b17e263c119d475dfe4f2bd17cfe61d623b9
Yago Ortiz <115984356+Kaynt-manage@users.noreply.github.com>
2023-12-04 18:12:27 +0100

    Tutoriales
    Se han añadido dos niveles de tutorial para enseñar más progresivamente, unque quedan pendientes de añadir imagenes explicativas y posiblemente quitar algo de texto.

--- Assets/Scripts/Behaviours/UI/TalkBehaviour.cs
+++ Assets/Scripts/Behaviours/UI/TalkBehaviour.cs
@@ -8,6 +8,7 @@ public class TalkBehaviour : MonoBehaviour
 {
     public string[] dialog;
     private int currentDialog=0;
+    public GameObject[] image;
     public TMP_Text text;
     public Button nextButton;
     // Start is called before the first frame update
@@ -20,6 +21,8 @@ public class TalkBehaviour : MonoBehaviour
             currentDialog++;
             if (currentDialog < dialog.Length)
             {
+                image[currentDialog].SetActive(true);
+                image[currentDialog-1].SetActive(false);
                 text.text = dialog[currentDialog];
             }
             else
