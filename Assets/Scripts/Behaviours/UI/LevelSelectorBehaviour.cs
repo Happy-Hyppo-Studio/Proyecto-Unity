b614c62ab55c1b6833ea6b2b1be43323d0938020
DanielBermejoValverde <43214261+DanielBermejoValverde@users.noreply.github.com>
2023-12-08 15:51:13 +0100

    WorldSelect
    Ahora en la pantalla de niveles se puede seleccionar entre distintos mundos

---
+++ Assets/Scripts/Behaviours/UI/LevelSelectorBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectorBehaviour : MonoBehaviour
+{
+    public GameObject[] worlds;
+    public Button right, left;
+    private int world0=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        right.onClick.AddListener(() => {
+            if (world0 < worlds.Length - 1)
+            {
+                world0++;
+                worlds[world0 - 1].SetActive(false);
+                worlds[world0].SetActive(true);
+            }
+        });
+        left.onClick.AddListener(() => {
+            if (world0 > 0)
+            {
+                world0--;
+                worlds[world0 + 1].SetActive(false);
+                worlds[world0].SetActive(true);
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
