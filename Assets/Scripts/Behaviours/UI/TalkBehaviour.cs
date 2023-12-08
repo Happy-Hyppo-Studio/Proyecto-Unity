0168b52dd1a365ee89d048ba858649d835be8e2f
DanWfram <100808263+DanWfram@users.noreply.github.com>
2023-12-08 20:43:11 +0100

    texto sin boton y con animacion y mejora de texto para visibilidad

--- Assets/Scripts/Behaviours/UI/TalkBehaviour.cs
+++ Assets/Scripts/Behaviours/UI/TalkBehaviour.cs
@@ -7,30 +7,54 @@ using UnityEngine.UI;
 public class TalkBehaviour : MonoBehaviour
 {
     public string[] dialog;
-    private int currentDialog=0;
-    public GameObject[] image;
     public TMP_Text text;
-    public Button nextButton;
+    Coroutine dialogue;
+    public float textSpeed = 0.3f;
+    private int dialogueI;
+    private int dialogueLength;
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0.0f;
-        text.text = dialog[currentDialog];
-        nextButton.onClick.AddListener(() => 
-        {
-            currentDialog++;
-            if (currentDialog < dialog.Length)
-            {
-                image[currentDialog - 1].SetActive(false);
-                image[currentDialog].SetActive(true);
-                text.text = dialog[currentDialog];
-            }
-            else
-            {
-                Time.timeScale = 1.0f;
-                Destroy(this.gameObject);
-            }
-        });
+        dialogue = StartCoroutine(StartDialogue());
+        dialogueLength = dialog.Length - 1;
     }
-
+    // Update is called once per frame
+    void Update()
+    {
+        //si el dialogo no se acabo mostrando completo
+        if (Input.GetMouseButtonDown(0) && text.text.Length < dialog[dialogueI].Length)
+        {
+            //saltar animacion del dialogo
+            if (dialogue != null) StopCoroutine(dialogue);
+            text.text = dialog[dialogueI];
+            return;
+        }
+        //si el dialogo acabo pero quedan mas dialogos
+        if (Input.GetMouseButtonDown(0) && dialogueI < dialogueLength && text.text.Length == dialog[dialogueI].Length)
+        {
+            //empezar nuevo dialogo
+            if (dialogue != null) StopCoroutine(dialogue);
+            dialogueI++;
+            dialogue = StartCoroutine(StartDialogue());
+            return;
+        }
+        //si no hay mas dialogos
+        if (Input.GetMouseButtonDown(0) && dialogueI >= dialogueLength)
+        {
+            Time.timeScale = 1.0f;
+            Destroy(this.gameObject);
+        }
+    }
+    IEnumerator StartDialogue()
+    {
+        text.text = "";
+        int letterI = 0;
+        while (letterI < dialog[dialogueI].Length)
+        {
+            text.text += dialog[dialogueI][letterI];
+            letterI++;
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
+    }  
 }
